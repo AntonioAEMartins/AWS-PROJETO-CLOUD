b65815f19a907bc3c3e34e2c27a0561f6f7d33d1
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2023-11-20 12:22:31 -0300

    Security Groups Update

--- sg/main.tf
+++ sg/main.tf
@@ -1,9 +1,35 @@
 module "vpc" {
-    source = "./vpc"
+    source = "../vpc"
 }
 
 resource "aws_security_group" "ec2_sg" {
     name = "ec2_sg"
+    vpc_id = module.vpc.vpc_id
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "lb_sg" {
+    name = "lb_sg"
     vpc_id = module.vpc.vpc_id
 
     ingress {
--- sg/output.tf
+++ sg/output.tf
@@ -1,3 +1,7 @@
 output "ec2_sg_id" {
     value = aws_security_group.ec2_sg.id
 }
+
+output "lb_sg_id" {
+    value = aws_security_group.lb_sg.id
+}
