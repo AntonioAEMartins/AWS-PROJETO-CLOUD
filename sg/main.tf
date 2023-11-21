f104f301c4b2b2e6df9a6e4aea98765b4b17e8db
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2023-11-20 22:29:04 -0300

    SG - VPC Correct

--- sg/main.tf
+++ sg/main.tf
@@ -1,10 +1,6 @@
-module "vpc" {
-    source = "../vpc"
-}
-
 resource "aws_security_group" "ec2_sg" {
     name = "ec2_sg"
-    vpc_id = module.vpc.vpc_id
+    vpc_id = var.vpc_id
 
     ingress {
         from_port = 80
@@ -30,7 +26,7 @@ resource "aws_security_group" "ec2_sg" {
 
 resource "aws_security_group" "lb_sg" {
     name = "lb_sg"
-    vpc_id = module.vpc.vpc_id
+    vpc_id = var.vpc_id
 
     ingress {
         from_port = 80
@@ -51,5 +47,31 @@ resource "aws_security_group" "lb_sg" {
         to_port = 0
         protocol = -1
         cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# Security Group for RDS that only allows traffic from the EC2 instance
+resource "aws_security_group" "rds_sg" {
+    name = "rds_sg"
+    description = "Security Group for RDS that only allows traffic from the EC2 instance"
+    vpc_id = var.vpc_id
+
+    ingress {
+        description = "Allow traffic from EC2 instance"
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        security_groups = [aws_security_group.ec2_sg.id]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "rds_sg"
     }
 }
