af54a93dee1d3fd0e7d3a55024594201bace997d
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2023-11-24 17:42:01 -0300

    nginx test

--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
+# variable "user_data" {
+#     description = "Initialization String for FastAPI"
+#     type = string
+#     default = "uvicorn sql_app.main:app"
+# }
+
 variable "user_data" {
     description = "Initialization String for FastAPI"
     type = string
-    default = "uvicorn sql_app.main:app"
+    default = <<-EOF
+        #!/bin/bash
+        sudo apt-get update
+        sudo apt-get install -y nginx
+        sudo systemctl enable nginx
+        sudo systemctl start nginx
+    EOF
 }
