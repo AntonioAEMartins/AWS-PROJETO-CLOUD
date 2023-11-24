bedd6067352052b498d07db397292a69c6765b1c
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2023-11-24 17:42:11 -0300

    vpc update

--- vpc/main.tf
+++ vpc/main.tf
@@ -10,6 +10,8 @@ resource "aws_subnet" "cloud_public_subnet" {
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
 
+    map_public_ip_on_launch = true
+
     tags = {
         name = "cloud_subnet"
     }
@@ -19,6 +21,8 @@ resource "aws_subnet" "cloud_public_subnet2" {
     vpc_id = aws_vpc.cloud_vpc.id
     cidr_block = "10.0.3.0/24"
     availability_zone = "us-east-1b"
+
+    map_public_ip_on_launch = true
 
     tags = {
         name = "cloud_subnet"
