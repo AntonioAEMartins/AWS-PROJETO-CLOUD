f4bf2f2039cf4a2ef824c8f9503e9010242efa9b
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2023-11-27 07:49:37 -0300

    iam ec2

--- iam/main.tf
+++ iam/main.tf
@@ -1,11 +1,54 @@
 resource "aws_iam_role" "cloud_iam_role" {
   name               = var.iam_role_name
-  assume_role_policy = var.iam_role_assume_role_policy
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_policy" "cloud_ec2_policy" {
   name   = var.iam_policy_name
-  policy = var.iam_policy_policy
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = [
+          "rds:Describe*",
+          # "cloudwatch:PutMetricData",
+          # "cloudwatch:GetMetricData",
+          # "cloudwatch:ListMetrics"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ],
+        Effect   = "Allow",
+        Resource = "*" // You may want to restrict this to specific secret ARNs
+      }
+    ]
+  })
 }
 
 resource "aws_iam_instance_profile" "cloud_instance_profile" {
--- iam/variables.tf
+++ iam/variables.tf
@@ -39,7 +39,7 @@ variable "iam_policy_policy" {
   "Statement": [
     {
       "Action": [
-        "ec2:*"
+        
       ],
       "Effect": "Allow",
       "Resource": "*"
