0628438a870278f812dd520b913cba57aaab1286
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2023-11-30 22:21:12 -0300

    Update output.tf

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -5,3 +5,11 @@ output "target_group_arn" {
 output "load_balancer_dns_name" {
     value = aws_lb.application_lb.dns_name
 }
+
+output "load_balancer_id" {
+    value = aws_lb.application_lb.id
+}
+
+output "load_balancer_name"{
+    value = aws_lb.application_lb.name
+}
